6957dc16d6e5863be7c354076c5787d58abaff61
Glitch (hello-sqlite) <none>
2019-02-21 21:13:21 +0000

    ☎️💼 Checkpoint ./public/scripts.js:2904201/645:3148053/76 ./views/index.html:2904201/24

--- public/scripts.js
+++ public/scripts.js
@@ -5,16 +5,13 @@ const restaurants = [
   'Old Spaghetti Factory',
 ];
 
-function pickplace(){
-  console.log();
-  // 1. pick random restaurnt from list
-  const restaurantName = randomRestaurant();
-  // 2. get handle to h1
-  const header = document.querySelector('#title');
-  // 3. set innerText of h1 to restaurnat
-}
-
 function randomRestaurant(){
   const index = Math.floor(Math.random() * restaurants.length);
   return restaurants[index];
 }
+
+document.querySelector('#choose-button').addEventListener('click', event => {
+  if (event.target.matches('#model > *')) {
+    do nothi
+  document.querySelector('#title').innerText = randomRestaurant();  
+});

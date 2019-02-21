9b37caf5e4cdccd32679f9dc58d28e38b8fca595
Glitch (what-to-eat) <none>
2019-02-21 22:25:35 +0000

    🌘🏀 Checkpoint ./public/scripts.js:2904201/176:3148053/219

--- public/scripts.js
+++ public/scripts.js
@@ -1,12 +1,9 @@
-async function randomRestaurant() {
-  // query api for random restaurant
-  const response = await fetch('');
-  
-  // GET /restaurants/random == {name: 'IHOP'}
-  const restaurant = null;
-  // return name of restaurant returned from the server
+async function randomRestaurantName() {
+  const response = await fetch('/restaurants/random');
+  const restaurant = await response.json();
+  return restaurant.name;
 }
 
 document.querySelector('#choose-button').addEventListener('click', async () => {
-  document.querySelector('#title').innerText = await randomRestaurant();  
+  document.querySelector('#title').innerText = await randomRestaurantName();  
 });

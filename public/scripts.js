43b205f0642ddaeff4db0056b6458254775d0e74
Glitch (what-to-eat) <none>
2019-02-23 23:15:01 +0000

    ✂️👯 Checkpoint ./public/scripts.js:3148053/568 ./public/style.css:3148053/129

--- public/scripts.js
+++ public/scripts.js
@@ -17,8 +17,8 @@ async function buildRestaurantList(){
   ul.innerHTML = ""; 
   list.forEach(function(item){ // for (const item of list) is my preferred format but it's purely a preference
     const li = document.createElement("li");
-    li.appendChild(document.createTextNode(item.name)); // li.textContent = item.name should work here as well
-    li.setAttribute("class", "not-editable"); // Alternatives would be: `li.className = 'not-editable'` or `li.classList.add('not-editable')`.
+    li.textContent = item.name; // li.textContent = item.name should work here as well
+    li.className = 'not-editable'; // Alternatives would be: `li.className = 'not-editable'` or `li.classList.add('not-editable')`.
     ul.appendChild(li);
   })  
 }
@@ -51,15 +51,15 @@ document.querySelector('#new-restaurant').addEventListener('submit', async funct
   await buildRestaurantList();
 });
 
-// Could just pass `buildRestaurantList` as the callback instead of creating a new function, e.g. `document.addEventListener('DOMContentLoaded', buildRestaurantList)`
-// The script tag in the page is also located after the HTML we depend on, so it's guaranteed to be parsed by the time this code runs, meaning we could also
-// just call buildRestaurantList directly
-document.addEventListener("DOMContentLoaded", async function() {
-  await buildRestaurantList();
-});
-
 document.body.addEventListener('click', function(event) {
-  if (document.querySelector('#restaurant-name').contains(event.target)){
+  if (document.querySelector('.editable').contains(event.target)){
     console.log("click");  
   }
 });
+
+// Could just pass `buildRestaurantList` as the callback instead of creating a new function, e.g. `document.addEventListener('DOMContentLoaded', buildRestaurantList)`
+// The script tag in the page is also located after the HTML we depend on, so it's guaranteed to be parsed by the time this code runs, meaning we could also
+// just call buildRestaurantList directly
+buildRestaurantList();
+
+

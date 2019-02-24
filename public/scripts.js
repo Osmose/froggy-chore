2bd4ae2498277aa75773675536efc6e5eabc761d
Glitch (what-to-eat) <none>
2019-02-24 23:33:22 +0000

    🐴📕 Checkpoint ./public/scripts.js:2904201/2662

--- public/scripts.js
+++ public/scripts.js
@@ -10,56 +10,66 @@ class API {
   }
   
   async fetch(path, options={}) {
-    const response = fetch(path, {
+    const response = await fetch(path, {
+      ...options,
       headers: {
-        'Authoriz
+        Authorization: this.password,
         ...options.headers,
       },
-      ...options,
     });
+    
+    if (!response.ok) {
+      throw new Error(await response.text());
+    }
+    
+    return response.json();
   }
   
   async getRestaurants() {
     return this.fetch('/restaurants');
   }
+  
+  async getRandomRestaurant() {
+    return this.fetch('/restaurants/random');
+  }
+  
+  async addRestaurant(name) {
+    return this.fetch('/restaurants/add', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+      }),
+    });
+  }
 }
 
-async function getRestaurants(){
-  const response = await fetch('/restaurants');
-  return response.json(); 
-} 
+let api = new API();
+const dom = {
+  chooseButton: document.querySelector('#choose-button'),
+  restaurantList: document.querySelector('#restaurant-list'),
+};
 
-// document.querySelector('#new-restaurant').addEventListener('submit', async function(event) {
-//   event.preventDefault();
+// When the choose button is clicked, get a random restaurant from the server
+// and show a short animation before highlighting it.
+dom.chooseButton.addEventListener('click', async () => {
+  const restaurant = await api.getRandomRestaurant();
   
-//   const formData = new FormData(this);
-//   const newRestaurant = {
-//     name: formData.get('name'),
-//   };
-  
-//   const response = await fetch('/restaurants/add', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Authorization': formData.get('password'),
-//     },
-//     body: JSON.stringify(newRestaurant),
-//   });
-//   if (!response.ok) {
-//     window.alert(await response.text());
-//   } else {
-//     window.alert(`${newRestaurant.name} added!`);
-//   }
-//   buildRestaurantList();
-// });
+  for (const li of dom.restaurantList.children) {
+    li.classList.remove('selected'); 
+  }
+  const restaurantLi = Array.from(dom.restaurantList.children).find(
+    li => li.textContent === restaurant.name,
+  );
+  restaurantLi.classList.add('selected');
+});
 
 // On load, build the restaurant list
 (async function() {
-  const api = new API();
-  const restaurantList = document.querySelector('#restaurant-list');
   for (const restaurant of await api.getRestaurants()) {
     const li = document.createElement('li');
     li.textContent = restaurant.name; 
-    restaurantList.appendChild(li);
+    dom.restaurantList.appendChild(li);
   }
 })();

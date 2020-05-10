449dec0e9a4f4f3d477a17c398f71be9262dea1d
Glitch (what-to-eat) <none>
2020-05-10 00:02:21 +0000

    🐚🐧 Checkpoint ./server.js:2904201/11423

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ db.serialize(() => {
     );
     console.log("New table lists created!");
   } else {
-    console.log('Database ready to go!');
+    console.log("Database ready to go!");
   }
 });
 
@@ -29,54 +29,34 @@ app.get("/", (request, response) => {
   response.sendFile(`${__dirname}/views/index.html`);
 });
 
-// endpoint to get all the dreams in the database
+// GET /api/list/:
 app.get("/api/list/${listId}", (request, response) => {
-  db.all("SELECT * from lists", (err, rows) => {
-    response.send(JSON.stringify(rows));
-  });
+  db.get(
+    "SELECT * FROM lists WHERE id = $id",
+    { $id: request.params.listId },
+    (err, list) => {
+      if (err) {
+        response.send(list.json);
+      } else {
+        response.sendStatus(500);
+      }
+    }
+  );
 });
 
 // endpoint to add a dream to the database
-app.post("/addDream", (request, response) => {
-  console.log(`add to dreams ${request.body.dream}`);
-
-  // DISALLOW_WRITE is an ENV variable that gets reset for new projects
-  // so they can write to the database
-  if (!process.env.DISALLOW_WRITE) {
-    const cleansedDream = cleanseString(request.body.dream);
-    db.run(`INSERT INTO Dreams (dream) VALUES (?)`, cleansedDream, error => {
+app.post("/api/list/${listId}", (request, response) => {
+  db.run(
+    `INSERT INTO lists (id, json) VALUES ($id, $json) ON CONFLICT(id) UPDATE SET json = $json`,
+    { $id: request.params.listId, $json: request.body.json },
+    (request, error) => {
       if (error) {
         response.send({ message: "error!" });
       } else {
         response.send({ message: "success" });
       }
-    });
-  }
-});
-
-// endpoint to clear dreams from the database
-app.get("/clearDreams", (request, response) => {
-  // DISALLOW_WRITE is an ENV variable that gets reset for new projects so you can write to the database
-  if (!process.env.DISALLOW_WRITE) {
-    db.each(
-      "SELECT * from Dreams",
-      (err, row) => {
-        console.log("row", row);
-        db.run(`DELETE FROM Dreams WHERE ID=?`, row.id, error => {
-          if (row) {
-            console.log(`deleted row ${row.id}`);
-          }
-        });
-      },
-      err => {
-        if (err) {
-          response.send({ message: "error!" });
-        } else {
-          response.send({ message: "success" });
-        }
-      }
-    );
-  }
+    }
+  );
 });
 
 const listener = app.listen(process.env.PORT, () => {

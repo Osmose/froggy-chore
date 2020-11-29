8abd444454a45d5caaf8dbec10ac4415a46b19e4
Glitch (froggy-chore) <none>
2020-11-29 17:10:19 +0000

    😘🌅 Checkpoint ./.glitch-assets:2904201/396 ./public/script.js:2904201/229 ./public/styles.css:2904201/209

--- public/script.js
+++ public/script.js
@@ -383,7 +383,10 @@ function App() {
 
   return html`
     <${ChoreContext.Provider} value=${choreInteractor}>
-      <h1><a href="/">Froggy Chore</a></h1>
+      <h1 class="header">
+        <a href="/">Froggy Chore</a>
+        <img class="froggy-rotated" src="https://cdn.glitch.com/59c2bae2-f034-4836-ac6d-553a16963ad6%2Ffroggy-chore-rotated.png?v=1606669566496" />
+      </h1>
       ${!listId ? html`
         <${Welcome} />
       ` : html`
